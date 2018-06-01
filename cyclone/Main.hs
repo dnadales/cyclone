8a6ad51f70bc3b9b898b5bb66eef2cfe1713e6bb
Damian Nadales <damian.nadales@gmail.com>
2018-06-01 12:51:00 +0200

    - Add command line arguments support to the main program. - Add functions for spawning either a master node or a slave node.

--- cyclone/Main.hs
+++ cyclone/Main.hs
@@ -1,6 +1,73 @@
+{-# LANGUAGE QuasiQuotes         #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
-import           Cyclone (runCyclone)
+import           Control.Monad         (unless)
+import           System.Console.Docopt (Arguments, Docopt, command, docopt,
+                                        exitWithUsage, getArgWithDefault,
+                                        isPresent, longOption, parseArgsOrExit)
+import           System.Environment    (getArgs)
+import           Text.Read             (readEither)
+
+import           Cyclone               (runCyclone, runCycloneSlave)
+
+
+patterns :: Docopt
+patterns = [docopt|
+cyclone
+
+Usage:
+  cyclone [-hpswr]
+  cyclone slave [-hp]
+
+Options:
+  -h --host <string>        Host in which the node should be started.
+
+  -p --port <number>        Port number in which the node should be started. If
+                            not present the default port 9090 will be chosen.
+                            Port numbers must be in the range 1024-65535.
+
+  -s, --send-for <secs>     Duration in seconds of the message sending interval.
+
+  -w, --wait-for <secs>     Duration in seconds of the grace period.
+
+  -r, --with-seed <seed>    Initial seed for the random number generator.
+|]
+
+defaultPort :: String
+defaultPort = "9090"
+
+defaultHost :: String
+defaultHost = "localhost"
+
+minPortValue :: Int
+minPortValue = 1024
+
+maxPortValue :: Int
+maxPortValue = 65535
 
 main :: IO ()
-main = runCyclone
+main = do
+    args  <- parseArgsOrExit patterns =<< getArgs
+    port  <- getPort args
+    -- Note that we don't validate the host string.
+    let host = getArgWithDefault args defaultHost (longOption "host")
+    if (args `isPresent` command "slave")
+       then runCycloneSlave host port
+       else runCyclone host port
+    where
+      getPort :: Arguments -> IO String
+      getPort args = do
+          let portStr = getArgWithDefault args defaultPort (longOption "port")
+          val <- case readEither portStr of
+              Left _ -> do
+                  putStrLn $ "Port number should be an integer. Got " ++ portStr
+                  exitWithUsage patterns
+              Right (val :: Int) ->
+                  return val
+          unless (minPortValue <= val && val <= maxPortValue) $ do
+              putStrLn $ "Invalid port number: " ++ show val
+                       ++ ", a port number should be in the range "
+                       ++ show minPortValue ++ " - " ++ show maxPortValue
+              exitWithUsage patterns
+          return portStr
--- src/Cyclone.hs
+++ src/Cyclone.hs
@@ -1,6 +1,54 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Cyclone
-    (runCyclone)
+    (runCyclone, runCycloneSlave)
 where
 
-runCyclone :: IO ()
-runCyclone = undefined
+import           Control.Distributed.Process                        (NodeId,
+                                                                     Process,
+                                                                     RemoteTable,
+                                                                     say, spawn,
+                                                                     terminate)
+import           Control.Distributed.Process.Backend.SimpleLocalnet (Backend, initializeBackend,
+                                                                     startMaster,
+                                                                     startSlave,
+                                                                     terminateAllSlaves)
+import           Control.Distributed.Process.Closure                (mkClosure,
+                                                                     remotable)
+import           Control.Distributed.Process.Node                   (initRemoteTable,
+                                                                     runProcess)
+import           Control.Monad                                      (forM)
+import           Control.Monad.IO.Class                             (liftIO)
+import           Network.Socket                                     (HostName,
+                                                                     ServiceName)
+
+import           Control.Concurrent                                 (threadDelay)
+
+cycloneNode ::Int ->  Process ()
+cycloneNode i = do
+    liftIO $ putStrLn $ "Hello, I got " ++ show i
+    say $ "Hello, I got " ++ show i
+
+remotable ['cycloneNode]
+
+myRemoteTable :: RemoteTable
+myRemoteTable = Cyclone.__remoteTable initRemoteTable
+
+runCyclone :: HostName
+           -> ServiceName
+           -> IO ()
+runCyclone host port = do
+    backend <- initializeBackend host port myRemoteTable
+    startMaster backend (master backend)
+
+master :: Backend -> [NodeId] -> Process ()
+master backend slaves = do
+    ps <- forM slaves $ \nid -> do
+        say $ "Starting slave on " ++ show nid
+        spawn nid $ $(mkClosure 'cycloneNode) (1 :: Int)
+    liftIO $ threadDelay 4000000
+    terminateAllSlaves backend
+
+runCycloneSlave :: HostName -> ServiceName -> IO ()
+runCycloneSlave host port = do
+    backend <- initializeBackend host port myRemoteTable
+    startSlave backend
